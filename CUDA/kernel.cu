07acb1aecb65a6a9fb206608fd9c6f7d7996f551
Francisco <matteomejia@github.com>
2019-06-25 23:33:03 -0500

    fixed

--- CUDA/kernel.cu
+++ CUDA/kernel.cu
@@ -14,7 +14,6 @@ __global__ void matrixMultiplicationKernel(float* A, float* B, float* C, int N) 
     float tmpSum = 0;
 
     if (ROW < N && COL < N) {
-        // each thread computes one element of the block sub-matrix
         for (int i = 0; i < N; i++) {
             tmpSum += A[ROW * N + i] * B[i * N + COL];
         }
@@ -25,8 +24,6 @@ __global__ void matrixMultiplicationKernel(float* A, float* B, float* C, int N) 
 
 void matrixMultiplication(float *A, float *B, float *C, int N){
 
-    // declare the number of blocks per grid and the number of threads per block
-    // use 1 to 512 threads per block
     dim3 threadsPerBlock(N, N);
     dim3 blocksPerGrid(1, 1);
         if (N*N > 512){
