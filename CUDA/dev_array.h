2ebe5f09d4ffa93a19d2e79b0af48bc00aaf6f97
Francisco <matteomejia@github.com>
2019-06-26 23:22:42 -0500

    every .out

--- CUDA/dev_array.h
+++ CUDA/dev_array.h
@@ -1,39 +1,45 @@
-#ifndef _ARRAY_H_
-#define _ARRAY_H_
+#ifndef _DEV_ARRAY_H_
+#define _DEV_ARRAY_H_
 
 #include <stdexcept>
 #include <algorithm>
 #include <cuda_runtime.h>
 
 template <class T>
-class array
+class dev_array
 {
+// public functions
 public:
-    explicit array()
+    explicit dev_array()
         : start_(0),
           end_(0)
     {}
 
-    explicit array(size_t size)
+    // constructor
+    explicit dev_array(size_t size)
     {
         allocate(size);
     }
-    ~array()
+    // destructor
+    ~dev_array()
     {
         free();
     }
 
+    // resize the vector
     void resize(size_t size)
     {
         free();
         allocate(size);
     }
 
+    // get the size of the array
     size_t getSize() const
     {
         return end_ - start_;
     }
 
+    // get data
     const T* getData() const
     {
         return start_;
@@ -44,31 +50,35 @@ public:
         return start_;
     }
 
+    // set
     void set(const T* src, size_t size)
     {
         size_t min = std::min(size, getSize());
         cudaError_t result = cudaMemcpy(start_, src, min * sizeof(T), cudaMemcpyHostToDevice);
-        if (result != cudaSuccess)
+        if (!result)
         {
             throw std::runtime_error("failed to copy to device memory");
         }
     }
+    // get
     void get(T* dest, size_t size)
     {
         size_t min = std::min(size, getSize());
         cudaError_t result = cudaMemcpy(dest, start_, min * sizeof(T), cudaMemcpyDeviceToHost);
-        if (result != cudaSuccess)
+        if (!result)
         {
             throw std::runtime_error("failed to copy to host memory");
         }
     }
 
 
+// private functions
 private:
+    // allocate memory on the device
     void allocate(size_t size)
     {
         cudaError_t result = cudaMalloc((void**)&start_, size * sizeof(T));
-        if (result != cudaSuccess)
+        if (!result)
         {
             start_ = end_ = 0;
             throw std::runtime_error("failed to allocate device memory");
@@ -76,6 +86,7 @@ private:
         end_ = start_ + size;
     }
 
+    // free memory on the device
     void free()
     {
         if (start_ != 0)
---
+++ CUDA/matrixmul.cu
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <cuda_runtime.h>
+#include "kernel.h"
+#include "kernel.cu"
+#include "dev_array.h"
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int N = 4096;
+    int SIZE = N*N;
+
+    vector<float> h_A(SIZE);
+    vector<float> h_B(SIZE);
+    vector<float> h_C(SIZE);
+
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
+            h_A[i*N+j] = sin(i);
+            h_B[i*N+j] = cos(j);
+        }
+    }
+
+    dev_array<float> d_A(SIZE);
+    dev_array<float> d_B(SIZE);
+    dev_array<float> d_C(SIZE);
+
+    d_A.set(&h_A[0], SIZE);
+    d_B.set(&h_B[0], SIZE);
+
+    matrixMultiplication(d_A.getData(), d_B.getData(), d_C.getData(), N);
+    cudaDeviceSynchronize();
+
+    d_C.get(&h_C[0], SIZE);
+    cudaDeviceSynchronize();
+
+    return 0;
+}
