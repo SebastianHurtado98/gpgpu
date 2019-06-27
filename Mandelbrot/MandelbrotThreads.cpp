fbcc253249b80d7b7f0799e99fb22cf0b727c25e
SebastianHurtado98 <sebastian.hurtado@utec.edu.pe>
2019-06-27 16:33:39 -0500

    reemplazo de del sfml por un ejemplo

--- Mandelbrot/MandelbrotThreads.cpp
+++ Mandelbrot/MandelbrotThreads.cpp
@@ -86,12 +86,51 @@ int main()
    }
    printf("\n");
 
+       const int bpp = 32;
+
+sf::RenderWindow window(sf::VideoMode(size, size, bpp), "Mandelbrot");
+    window.setVerticalSyncEnabled(true);
 
 
+    std::vector<sf::CircleShape> points;
+
+    for (int i=0; i<size; i++){
+        for(int j = 0; j < size; j++)
+        {
+            sf::CircleShape ball(1);
+            ball.setFillColor(sf::Color::White);
+            ball.setOrigin(1 / 2, 1 / 2);
+            ball.setPosition(i,j);
+            points.push_back(ball);
+        }
+    }
+
+    sf::Clock clock;
+    sf::Time elapsed = clock.restart();
+    const sf::Time update_ms = sf::seconds(1.f / 120.f);
+    while (window.isOpen()) {
+
+        sf::Event event;
+        while (window.pollEvent(event)) {
+
+            if ((event.type == sf::Event::Closed) ||
+                ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))) {
+                window.close();
+                break;
+            }
+        }
 
 
+        window.clear();
+        for (int i=0; i<totalElements; i++) {
+                if(matrixC[i] == 1) points[i].setFillColor(sf::Color::Black);
+                else points[i].setFillColor(sf::Color::White);
+                window.draw(points[i]);
+        }
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
 
 
-   
-    return 0; 
 } 
--- Mandelbrot/main.cpp
+++ Mandelbrot/main.cpp
@@ -1,141 +1,151 @@
 #include <SFML/Graphics.hpp>
-#include <random>
-#include <functional>
-#include <cstdlib>
 #include <cmath>
-#include <vector>
 #include <random>
-#include <bits/stdc++.h> 
 
+const int IMAGE_WIDTH = 1000;
+const int IMAGE_HEIGHT = 600;
+double zoom = 0.004; // allow the user to zoom in and out...
+double offsetX = -0.7; // and move around
+double offsetY = 0.0;
+const int MAX = 100; // maximum number of iterations for mandelbrot()
+                     // don't increase MAX or the colouring will look strange
 
-#define MAX_THREAD 4 
+int mandelbrot(double, double, int);
+sf::Color getColor(int);
 
-float *matrixA = NULL;
-float *matrixB = NULL;
-int *matrixC = NULL;
-int iterations = 100;
+int main() {
+    sf::RenderWindow window(sf::VideoMode(IMAGE_WIDTH, IMAGE_HEIGHT), "Mandelbrot");
+    window.setFramerateLimit(30);
 
-int fixer = 0; 
-int size;
+    sf::Image image;
+    image.create(IMAGE_WIDTH, IMAGE_HEIGHT, sf::Color(0, 0, 0));
+    sf::Texture texture;
+    sf::Sprite sprite;
 
-void elementsA(float* data, int size)
-{
+    bool stateChanged = true; // track whether the image needs to be regenerated
 
-    int half = size/2;
-    for (int j = 0; j < size; j ++){
-        for (int i = 0; i<half; i++) data[(j * size) + i] = float(((-2.0) *(half-i))/half);
-        for (int i = half; i < size; i++) data[(j * size) + i] = float((2.0 *(i-half))/half);
-    }
-}
-void elementsB(float* data, int size)
-{
-
-    int half = size/2;
-    for (int j = 0; j < size; j ++){
-        for (int i = 0; i<half; i++) data[(i * size) + j] = float((2.0 *(half-i))/half);
-        for (int i = half; i < size; i++) data[(i * size) + j] = float(((-2.0) *(i-half))/half);
-    }
-}
-
-
-void* Mandelbrot(void* arg) 
-{ 
-    int core = fixer++; 
-    int totalSize = size * size;
-    int Z = 0;
-    int C;
-
-    for (int i = core * totalSize / 4; i < (core + 1) * totalSize / 4; i++)  {
-        Z = 0;
-        C = (matrixA[i] * matrixA[i]) + (matrixB[i] * matrixB[i]);
-        for (int j = 0; j < iterations; j++){
-            Z = (Z*Z) + C;
-        }
-        if ( Z > 2 ) matrixC[i] = 1;
-        else matrixC[i] = 0;
-    }
-} 
-
-
-
-int main()
-{
-    int size = 512;
-
-	int totalElements = size*size;
-
-	size_t datasizeTotal = sizeof(float)*totalElements;
-
-	matrixA = (float*)malloc(datasizeTotal);
-	matrixB = (float*)malloc(datasizeTotal);
-	matrixC = (int*)malloc(datasizeTotal);
-
-	elementsA(matrixA, size);
-	elementsB(matrixB, size);
-  
-
-    pthread_t threads[MAX_THREAD]; 
-  
-    for (int i = 0; i < MAX_THREAD; i++) { 
-        int* p; 
-        pthread_create(&threads[i], NULL, Mandelbrot, (void*)(p)); 
-    } 
-  
-
-    for (int i = 0; i < MAX_THREAD; i++)  
-        pthread_join(threads[i], NULL);     
-
-    const int totalSize = totalElements;
-    const int bpp = 32;
-    
-      printf("\n\nMatrix C: \n");
-   for(int i = 0; i < totalElements; i++)
-   {
-      printf("%d ", matrixC[i]);
-      if(((i + 1) % size) == 0) printf("\n");
-   }
-
-    sf::RenderWindow window(sf::VideoMode(size, size, bpp), "Mandelbrot");
-    window.setVerticalSyncEnabled(true);
-
-
-    std::vector<sf::CircleShape> points;
-
-    for (int i=0; i<size; i++){
-        for(int j = 0; j < size; j++)
-        {
-            sf::CircleShape ball(1);
-            ball.setFillColor(sf::Color::White);
-            ball.setOrigin(1 / 2, 1 / 2);
-            ball.setPosition(i,j);
-            points.push_back(ball);
-        }
-    }
-
-    sf::Clock clock;
-    sf::Time elapsed = clock.restart();
-    const sf::Time update_ms = sf::seconds(1.f / 120.f);
     while (window.isOpen()) {
-
         sf::Event event;
         while (window.pollEvent(event)) {
-
-            if ((event.type == sf::Event::Closed) ||
-                ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))) {
-                window.close();
-                break;
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    stateChanged = true; // image needs to be recreated when the user changes zoom or offset
+                    switch (event.key.code) {
+                        case sf::Keyboard::Escape:
+                            window.close();
+                            break;
+                        case sf::Keyboard::Q:
+                            zoom *= 0.9;
+                            break;
+                        case sf::Keyboard::E:
+                            zoom /= 0.9;
+                            break;
+                        case sf::Keyboard::W:
+                            offsetY -= 40 * zoom;
+                            break;
+                        case sf::Keyboard::S:
+                            offsetY += 40 * zoom;
+                            break;
+                        case sf::Keyboard::A:
+                            offsetX -= 40 * zoom;
+                            break;
+                        case sf::Keyboard::D:
+                            offsetX += 40 * zoom;
+                            break;
+                        default: break;
+                    }
+                default:
+                    break;
             }
         }
 
 
-        window.clear();
-        for (int i=0; i<totalSize; i++) {
-                if(matrixC[i] == 1) points[i].setFillColor(sf::Color::Black);
-                else points[i].setFillColor(sf::Color::White);
-                window.draw(points[i]);
+        if (stateChanged) { // only generate a new image if something has changed, to avoid unnecessary lag
+
+            for (int x = 0; x < IMAGE_WIDTH; x++) {
+                for (int y = 0; y < IMAGE_HEIGHT; y++) {
+                    // convert x and y to the appropriate complex number
+                    double real = (x - IMAGE_WIDTH / 2.0) * zoom + offsetX;
+                    double imag = (y - IMAGE_HEIGHT / 2.0) * zoom + offsetY;
+                    int value = mandelbrot(real, imag, MAX);
+                    image.setPixel(x, y, getColor(value));
+                }
+            }
+
+
+            texture.loadFromImage(image);
+            sprite.setTexture(texture);
         }
+
+
+        window.clear();
+        window.draw(sprite);
         window.display();
+
+        stateChanged = false;
     }
 
-    return EXIT_SUCCESS;
+    return 0;
+}
+
+int mandelbrot(double startReal, double startImag, int maximum) {
+    int counter = 0;
+    double zReal = startReal;
+    double zImag = startImag;
+    double nextRe;
+
+    while (pow(zReal, 2.0) + pow(zImag, 2.0) <= 4.0 && counter <= maximum) {
+        nextRe = pow(zReal, 2.0) - pow(zImag, 2.0) + startReal;
+        zImag = 2.0 * zReal * zImag + startImag;
+        zReal = nextRe;
+        if (zReal == startReal && zImag == startImag) { // a repetition indicates that the point is in the Mandelbrot set
+            return -1; // points in the Mandelbrot set are represented by a return value of -1
+        }
+        counter += 1;
+    }
+
+    if (counter >= maximum) {
+        return -1; // -1 is used here to indicate that the point lies within the Mandelbrot set
+    } else {
+        return counter; // returning the number of iterations allows for colouring
+    }
+}
+
+sf::Color getColor(int iterations) {
+    int r, g, b;
+
+    if (iterations == -1) {
+        r = 0;
+        g = 0;
+        b = 0;
+    } else if (iterations == 0) {
+        r = 255;
+        g = 0;
+        b = 0;
+    } else {
+        // colour gradient:      Red -> Blue -> Green -> Red -> Black
+        // corresponding values:  0  ->  16  ->  32   -> 64  ->  127 (or -1)
+        if (iterations < 16) {
+            r = 16 * (16 - iterations);
+            g = 0;
+            b = 16 * iterations - 1;
+        } else if (iterations < 32) {
+            r = 0;
+            g = 16 * (iterations - 16);
+            b = 16 * (32 - iterations) - 1;
+        } else if (iterations < 64) {
+            r = 8 * (iterations - 32);
+            g = 8 * (64 - iterations) - 1;
+            b = 0;
+        } else { // range is 64 - 127
+            r = 255 - (iterations - 64) * 4;
+            g = 0;
+            b = 0;
+        }
+    }
+
+    return sf::Color(r, g, b);
 }
